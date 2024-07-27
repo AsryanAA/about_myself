6aa50c6fd20c395baed164d0f3b29fe61cefc574
AsryanAA <aramik16spartak@mail.ru>
2024-07-27 22:45:59 +0300

    AM-1_create_component_header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
+    import Header from './components/Header.vue'
 </script>
 
 <template>
-    <h1>About myself</h1>
+    <div class='container'>
+        <Header />
+        About myself
+    </div>
 </template>
 
 <style scoped>
+    .container {
+        width: 80%;
+        margin: 3.5rem auto auto auto;
+        background-color: rgb(255 255 255);
+        height: 100vh;
+        border-radius: 0.75rem;
+        box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1);
+    }
 </style>
